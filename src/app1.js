313e6b33d39ebed20209152b6ecebd12a7415183
kongyi <jerrykongyi1997@gmail.com>
2020-08-24 23:41:07 +0800

    mvc => Vue

--- src/app1.js
+++ src/app1.js
@@ -1,71 +1,42 @@
 import "./app1.css";
-import $ from "jquery";
-import Model from "./base/Model.js";
-import View from "./base/View.js";
-import EventBus from "./base/EventBus.js";
-
-// 数据相关放到 M
-const m = new Model({
-  data: {
-    // 初始化数据
-    n: parseFloat(localStorage.getItem("n")),
-  },
-  update: function (data) {
-    Object.assign(m.data, data);
-    m.trigger("m:updated");
-    localStorage.setItem("n", m.data.n);
-  },
-});
+import Vue from "vue";
 
 // 其他放到 C
 const init = (el) => {
-  new View({
+  new Vue({
     el: el,
-    data: m.data,
-    html: `
-      <div>
+    data: { n: parseFloat(localStorage.getItem("n")) },
+    methods: {
+      add() {
+        this.n += 1;
+      },
+      minus() {
+        this.n -= 1;
+      },
+      mul() {
+        this.n *= 2;
+      },
+      div() {
+        this.n /= 2;
+      },
+    },
+    watch: {
+      n: function () {
+        localStorage.setItem("n", this.n);
+      },
+    },
+    template: `
+      <section>
         <div id="output">
           <span id="number">{{n}}</span></div>
         <div id="actions">
-          <button id="add1">+1</button>
-          <button id="minus1">-1</button>
-          <button id="mul2">*2</button>
-          <button id="divide2">÷2</button>
+          <button @click="add">+1</button>
+          <button @click="minus">-1</button>
+          <button @click="mul">*2</button>
+          <button @click="div">÷2</button>
         </div>
-      </div>
+      </section>
     `,
-    render(data) {
-      const n = data.n;
-      if (this.el.children.length !== 0) this.el.empty();
-      $(this.html.replace("{{n}}", n)).appendTo(this.el);
-    },
-    events: {
-      "click #add1": "add",
-      "click #minus1": "minus",
-      "click #mul2": "mul",
-      "click #divide2": "div",
-    },
-    add() {
-      m.update({ n: m.data.n + 1 });
-    },
-    minus() {
-      m.update({ n: m.data.n - 1 });
-    },
-    mul() {
-      m.update({ n: m.data.n * 2 });
-    },
-    div() {
-      m.update({ n: m.data.n / 2 });
-    },
-    autoBindEvents() {
-      for (let key in this.events) {
-        const value = this[this.events[key]];
-        const spaceIndex = key.indexOf(" ");
-        const part1 = key.slice(0, spaceIndex);
-        const part2 = key.slice(spaceIndex + 1);
-        this.el.on(part1, part2, value);
-      }
-    },
   });
 };
 export default init;
